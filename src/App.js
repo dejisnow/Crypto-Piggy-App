103135eaa0bc7b5966f457aca01238ff01b0c75d
DEJI_SNOW <deji.snow@gmail.com>
2023-08-11 10:45:17 +0100

    404 page commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Routes , Route} from 'react-router-dom';
 import './piggyapp.css';
 import { Account } from './nav pages/account';
 import { Home } from './nav pages/home';
@@ -6,6 +6,7 @@ import { Invest } from './nav pages/invest';
 import { Nav } from './nav pages/nav';
 import { Learn } from './nav pages/learn folder/Learn';
 import { Savings } from './nav pages/savings';
+import { Errorpage } from './nav pages/error page/error';
 
 
 
@@ -26,6 +27,7 @@ function App() {
     <Route path='/investments' element= {<Invest />} />
     <Route path='/learn' element= {<Learn />} />
     <Route path='/account' element= {<Account />} />
+    <Route path='*' element={<Errorpage />} />
   </Routes>
 </Router>
 
---
+++ src/nav pages/error page/error.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export  function Errorpage() {
+  return (
+
+    <div className='error__page'>
+
+   
+    <div>404 Page Not Found</div>
+
+    </div>
+  )
+}
